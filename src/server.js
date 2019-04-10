91635dcb3477ec44d9f4c63681afeca2fee4195c
Daniel Felizardo <daniel.david772@gmail.com>
2019-04-09 23:08:00 -0300

    modificando o variavel port

--- src/server.js
+++ src/server.js
@@ -27,11 +27,9 @@ app.use((req, res, next) => {
   return next();
 });
 
-const port = process.env.PORT || 3333;
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 app.use(require("./routes"));
 
-app.listen(port);
+app.listen(process.env.PORT || 3333);
