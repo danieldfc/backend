13b7d746b91f3af4c21306830bda3a56685e54ee
Daniel Felizardo <daniel.david772@gmail.com>
2019-04-11 00:03:37 -0300

    Trocando a funcionalidade do express para a porta server http

--- src/server.js
+++ src/server.js
@@ -32,4 +32,4 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 app.use(require("./routes"));
 
-app.listen(process.env.PORT || 3333);
+server.listen(process.env.PORT || 3333);
